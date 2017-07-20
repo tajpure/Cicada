aa54c4e1a524d242a4a9f1e0c999d471a2aa7367
tajpure <tajpure@gmail.com>
2017-07-20 22:31:53 +0800

    add an very simple http response

--- src/server.rs
+++ src/server.rs
@@ -20,9 +20,16 @@ pub fn listen(port: String) {
                 },
             };
             println!("{}", String::from_utf8(buf.to_vec()).unwrap());
-            match stream.write(&buf) {
+            let response = b"HTTP/1.1 200 OK
+Date: Thu, 20 Jul 2017 05:41:19 GMT
+Content-length: 2
+Content-type: application/json
+
+ok";
+
+            match stream.write(response) {
                 Err(_) => break,
-                Ok(_) => continue,
+                Ok(_) => break,
             }
         }
     }
